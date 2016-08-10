1281f63f7412baf85d7de4f061be6e21f64ce1dc
Roger Zhang <roger.zhang.eng@gmail.com>
2016-08-10 15:05:49 +1000

    InterAppComm v2.0 session expired and user info sending OK

--- OneFillSim/ViewController.swift
+++ OneFillSim/ViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var contentTextView: UITextView!
+    
+    var userDataJSON: JSON?
     
     var inputText: String = ""
     
@@ -44,25 +47,45 @@ class ViewController: UIViewController, UITextViewDelegate {
         IACManager.sharedManager().handleAction("loginFromOnebank", withBlock: { inputParameters, success, failure in
         
             if success != nil {
-                do {
                     
+                    //var userData : JSON = inputParameters["userData"]! as AnyObject
                     print("Get loginFromOnebank url parameters: \(inputParameters)")
                     
+                    let userDataString = inputParameters["userData"] as! String
                     
+                    if let userDataDictionary = self.convertStringToDictionary(userDataString) {
+                        self.userDataJSON = JSON(userDataDictionary)
+                        let userName = self.userDataJSON!["user"]["profile"]["first_name"]
+                        print("\(userName.string) send message from Onebank")
+                    }
+                
                     //reply data back to URL sender
                     /*let jsonData = try NSJSONSerialization.dataWithJSONObject(["loginOkay"], options:[])
                     
                     let jsonStr = NSString(data: jsonData, encoding: NSUTF8StringEncoding) as! String
                     success(["reply":jsonStr],false)*/
                     
-                } catch {
-                    print("JSON serialization failed: \(error)")
-                }
+
             }
         
         
         })
         
+    }
+    
+    //construct JSON string to dictionary
+    func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+        if let data = text.dataUsingEncoding(NSUTF8StringEncoding) {
+        //data(using: String.Encoding.utf8) {
+            do {
+                return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String:AnyObject]
+                    
+                    //JSONSerialization.jsonObject(with: data, options: []) as? [String:AnyObject]
+            } catch let error as NSError {
+                print("Convert String to Dictionary Error: \(error.localizedDescription)")
+            }
+        }
+        return nil
     }
 
 }
